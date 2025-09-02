d1c5095dbc0feb936d249a7839c7f4572e9c9cbb
SamkelisiweKhanyile <126682088+SamkelisiweKhanyile@users.noreply.github.com>
2025-09-02 02:32:43 +0200

    Update CategoryService.js

--- src/api/business/CategoryService.js
+++ src/api/business/CategoryService.js
@@ -1,3 +1,31 @@
+// src/services/CategoryService.js
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/formule/category";
+
 export default {
-  getCategories() { return [{ id:1, name:'Electronics' }, { id:2, name:'Books' }]; }
+  
+  getAllCategories() {
+    return axios.get(`${API_URL}/getall`);
+  },
+  getCategoryById(id) {
+    return axios.get(`${API_URL}/read/${id}`);
+  },
+  createCategory(category) {
+    return axios.post(`${API_URL}/create`, category);
+  },
+  updateCategory(category) {
+    return axios.put(`${API_URL}/update`, category);
+  },
+  deleteCategory(id) {
+    return axios.delete(`${API_URL}/delete/${id}`);
+  },
+  searchByName(name) {
+    return axios.get(`${API_URL}/search/name/${name}`);
+  },
+  searchByDescription(keyword) {
+    return axios.get(`${API_URL}/search/description`, {
+      params: { keyword },
+    });
+  },
 };
